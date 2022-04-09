20c90b0f81ae91b9a60abec110c8b60fa7300d02
Elizabeth Teresa <69443070+elizabeth171717@users.noreply.github.com>
2022-04-09 02:45:18 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Element .getBoundingClientREact() method returs the size of
+// an element and its position relative to the viewport.
+
+// set date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
